7e155d2c687a255da37ab9636bd443a10b9650c9
john <johnmaluki58@gmail.com>
2023-08-26 17:59:30 +0300

    Add object relationship mapping

--- models/customer.py
+++ models/customer.py
@@ -1,3 +1,5 @@
+from review import Review
+
 class Customer:
     customers = []
     def __init__(self, first_name, last_name):
@@ -27,6 +29,23 @@ class Customer:
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}" 
+    
+    def restaurants(self):
+        """
+            Returns a **unique** list of all restaurants a customer has reviewed
+        """
+        reviews = [review for review in Review.reviews if review.customer.full_name() == self.full_name()]
+        unique_restaurant_names = set([review.restaurant.name for review in reviews])
+        unique_restaurant = [review.restaurant for review in reviews if review.restaurant.name in unique_restaurant_names]
+        return unique_restaurant
+    
+    def add_review(self, restaurant, rating):
+        """
+            Given a **restaurant object** and a star rating 
+            (as an integer), creates a new review and associates 
+            it with that customer and restaurant.
+        """
+        Review(self, restaurant, rating)
 
     @classmethod
     def add_customer(cls, customer):
--- models/restaurant.py
+++ models/restaurant.py
@@ -1,3 +1,5 @@
+from review import Review
+
 class Restaurant:
     def __init__(self, name):
         """
@@ -6,4 +8,22 @@ class Restaurant:
         self.__name = name
 
     def name(self):
-        return self.__name     
+        return self.__name
+
+    def reviews(self):
+        """
+            Returns a list of all reviews for that restaurant
+        """
+        return [review for review in Review.reviews if review.restaurant.name() == self.__name]
+    
+    def customers(self):
+        """
+            Returns a **unique** list of all customers who have reviewed a particular restaurant
+        """
+        unique_customer_names = set(
+            [review.customer.full_name() for review in self.reviews()])
+        unique_customers = [
+            review.customer for review in self.reviews if review.customer.full_name() in unique_customer_names
+        ]
+        return unique_customers
+    
